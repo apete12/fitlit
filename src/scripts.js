88e036b9901c1154547133c709eb95abd53f9e01
Prissilla Escobar <prissilla988@gmail.com>
2023-07-09 10:50:23 -0600

    Uncomment our imports

--- src/scripts.js
+++ src/scripts.js
@@ -3,12 +3,12 @@
 
 
 // An example of how you tell webpack to use a CSS file
-// import './css/styles.css';
+import './css/styles.css';
 
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png';
+import './images/turing-logo.png';
 
 // // An example of how you tell webpack to use a JS file
 import userData from './data/users';
