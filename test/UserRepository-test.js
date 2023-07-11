a3b5cdc21977890fcfe226a44af9dec0902d9248
Scotty Brown <scottybrown98@gmail.com>
2023-07-11 16:10:59 -0500

    Add testing for getOzByDay function

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -48,13 +48,13 @@ describe('User Repository', () => {
 
   it.skip('should return user object by id', function () {
 
-    // const userOne = getUserData(1, sampleData)
-    // const userTwo = getUserData(2, sampleData)
-
-    // expect(userOne[0].name).to.equal("Trystan Gorczany");
-    // expect(userTwo[0].name).to.equal("Tyreek VonRueden");
-
     const userOne = getUserData(1)
+    const userTwo = getUserData(2)
+
+    expect(userOne[0].name).to.equal("Trystan Gorczany");
+    expect(userTwo[0].name).to.equal("Tyreek VonRueden");
+
+    // const userOne = getUserData(1)
     // const userTwo = getUserData(2)
     
     expect(sampleDataObject.sampleData).to.equal(userOne);
@@ -73,7 +73,7 @@ describe('User Repository', () => {
   
 //////////////// getAvgDailyOunces test///////////////////////
 
-it.skip('should return average daily ounces for user', () => {
+it('should return average daily ounces for user', () => {
   const hydrationData = [
     {
       "userID": 1,
@@ -102,8 +102,42 @@ it.skip('should return average daily ounces for user', () => {
     },
   ]
 
-  const user2Avg = getAvgDailyOunces(2)
+  const user2Avg = getAvgDailyOunces(2, hydrationData)
   expect(user2Avg).to.equal(41)
+
+})
+
+it('should return number of OZ for a specific day', () => {
+  const hydrationData = [
+    {
+      "userID": 1,
+      "date": "2023/03/23",
+      "numOunces": 28
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 35
+    },
+    {
+      "userID": 3,
+      "date": "2023/03/24",
+      "numOunces": 95
+    },
+    {
+      "userID": 1,
+      "date": "2023/03/24",
+      "numOunces": 74
+    },
+    {
+      "userID": 2,
+      "date": "2023/03/24",
+      "numOunces": 47
+    },
+  ]
+
+  const userOneMarch24OZ = getOzByDay(1, "2023/03/24")
+  expect(userOneMarch24OZ).to.equal(74)
 
 })
 
