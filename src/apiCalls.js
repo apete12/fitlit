f713260332ec18bd41fd6ac338582a0d4b8385c3
Alex Peterson <aspeterson.126@gmail.com>
2023-07-12 20:12:04 -0500

    Add fetchUserData function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,5 +5,49 @@
 //     .then a
 // }
 
-console.log('I will be a fetch request!')
 
+export const fetchUserData = (dataType) => { 
+    fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log("ERROR", error));
+};
+
+
+
+// export const fetchUserData = (dataType) => { 
+    // fetch(`https://fitlit-api.herokuapp.com/api/v1/${dataType}`)
+    // .then(res => res.json())
+    // .then(data => {
+        // console.log(data)
+    // })
+    // .catch(error => console.log("ERROR", error));
+// };
+
+// export const getUserData = () => {
+    // // return Promise.all([fetchUserData('users'), fetchUserData('sleep'), fetchUserData('hydration'), fetchUserData('activity')])
+// }
+
+// const bigAssObject = {
+    // userData: fetchUserData(userData),
+    // hydrationData: fetchUserData(hydrationData),
+    // sleepData: fetchUserData(sleepData),
+    // activityData: fetchUserData(activityData)
+// }
+// 
+
+
+// fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+
+
+// function dosomethingapi() {
+//   //checkj out promise all
+//   apiCall(url)
+//   .then(data, () => {
+//     getUserData(data)
+//   }
+// }
