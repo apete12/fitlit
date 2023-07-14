a191b9c29145eee5c3f7446dc8b7031712ce4b87
Adrian Zabolitzki <adrianzab@gmail.com>
2023-07-13 19:11:37 -0600

    Add test for calculateUserAvgDailyHoursSlept

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { getUserData, getAvgSteps, getAvgDailyOunces, getOzByDay, calculateWeeklyOunces } from '../src/dataModel';
+import { getUserData, getAvgSteps, getAvgDailyOunces, getOzByDay, calculateWeeklyOunces, calculateUserAvgDailyHoursSlept } from '../src/dataModel';
 import { sampleData, sampleDataHydration, weeklyDataSample } from '../src/data/sampleData';
 
 
@@ -61,3 +61,37 @@ describe('User Repository', () => {
 
 });
 
+describe('Should test sleepData', () => {
+
+  const sampleSleepData = { sleepData: [
+    {userID: 1, date: '2023/03/21', hoursSlept: 9.9, sleepQuality: 4.7},
+    {userID: 2, date: '2023/03/21', hoursSlept: 7.4, sleepQuality: 1.7},
+    {userID: 1, date: '2023/03/22', hoursSlept: 6.3, sleepQuality: 3.6},
+    {userID: 2, date: '2023/03/22', hoursSlept: 4.2, sleepQuality: 4.6},
+    {userID: 1, date: '2023/03/23', hoursSlept: 10, sleepQuality: 2.4},
+    {userID: 2, date: '2023/03/23', hoursSlept: 6.7, sleepQuality: 2.9},
+    {userID: 1, date: '2023/03/24', hoursSlept: 9.6, sleepQuality: 4.3},
+    {userID: 2, date: '2023/03/24', hoursSlept: 8.4, sleepQuality: 3.5},
+    {userID: 1, date: '2023/03/25', hoursSlept: 9.7, sleepQuality: 4.7},
+    {userID: 2, date: '2023/03/25', hoursSlept: 4.7, sleepQuality: 3},
+    {userID: 1, date: '2023/03/26', hoursSlept: 8, sleepQuality: 3.1},
+    {userID: 2, date: '2023/03/26', hoursSlept: 4.2, sleepQuality: 1.2},
+    {userID: 1, date: '2023/03/27', hoursSlept: 4.1, sleepQuality: 3.9},
+    {userID: 2, date: '2023/03/27', hoursSlept: 9.2, sleepQuality: 1.6},
+    {userID: 1, date: '2023/03/28', hoursSlept: 4.8, sleepQuality: 2.5},
+    {userID: 2, date: '2023/03/28', hoursSlept: 7.2, sleepQuality: 2.2},
+    {userID: 1, date: '2023/03/29', hoursSlept: 7.2, sleepQuality: 1},
+    {userID: 2, date: '2023/03/29', hoursSlept: 4, sleepQuality: 3.1},
+    {userID: 1, date: '2023/03/30', hoursSlept: 6.5, sleepQuality: 1.4},
+    {userID: 2, date: '2023/03/30', hoursSlept: 5.3, sleepQuality: 4.7},
+  ]}
+
+  it('Should be able to calculate the average daily hours of sleep', () => {
+    expect(calculateUserAvgDailyHoursSlept(1, sampleSleepData)).to.equal(7.609999999999999)
+    expect(calculateUserAvgDailyHoursSlept(2, sampleSleepData)).to.equal(6.13)
+  })
+
+  // it('Should be able to calculate the average sleep quality per day over all time', () => {
+  //   expect()
+  // }
+})
