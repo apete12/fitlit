6b438bc641a4a2060283be47cf0121f9c4f5e6ee
Scotty Brown <scottybrown98@gmail.com>
2023-07-18 15:11:04 -0500

    Remove commented out sections from api file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,3 @@
-// Your fetch requests will live here!
-// export function apiCall(url) {
-//   return  fetch(url)
-//     .then JSON
-//     .then a
-// }
-
-
 export const fetchUserData = (dataType) => { 
    return fetch(`https://fitlit-api.herokuapp.com/api/v1/${dataType}`)
     .then(res => res.json())
@@ -17,36 +9,3 @@ export const promises = [
     fetchUserData('sleep'),
     fetchUserData('activity')
 ]
-
-
-
-
-
-// export const fetchUserData = (dataType) => { 
-    // fetch(`https://fitlit-api.herokuapp.com/api/v1/${dataType}`)
-    // .then(res => res.json())
-    // .then(data => {
-        // console.log(data)
-    // })
-    // .catch(error => console.log("ERROR", error));
-// };
-
-// export const getUserData = () => {
-    // // return Promise.all([fetchUserData('users'), fetchUserData('sleep'), fetchUserData('hydration'), fetchUserData('activity')])
-// }
-
-// const bigAssObject = {
-    // userData: fetchUserData(userData),
-    // hydrationData: fetchUserData(hydrationData),
-    // sleepData: fetchUserData(sleepData),
-    // activityData: fetchUserData(activityData)
-// }
-// 
-
-// function dosomethingapi() {
-//   //checkj out promise all
-//   apiCall(url)
-//   .then(data, () => {
-//     getUserData(data)
-//   }
-// }
