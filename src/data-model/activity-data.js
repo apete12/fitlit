5727ba25f7c66d6d0c191717b871a403e8d49f8d
Alex Peterson <aspeterson.126@gmail.com>
2023-07-25 14:36:28 -0500

    Refactor activity-data.js to create getUserActivtyLog function, eliminate from other fns to increase DRYness

--- src/data-model/activity-data.js
+++ src/data-model/activity-data.js
@@ -1,51 +1,55 @@
-const getDailySteps = (id, day, dataList) => {
+const getUserActivtyLog = (id, day, dataList) => {
   let activityLog = dataList.activityData 
-  let dailySteps = activityLog.find(log => log.userID === id && log.date === day)
-  if (!dailySteps) {
+  let userActivityEntry = activityLog.find(log => log.userID === id && log.date === day)
+  return userActivityEntry
+}
+
+
+const getDailySteps = (id, day, dataList) => {
+  let userActivityEntry = getUserActivtyLog(id, day, dataList)
+  if (!userActivityEntry) {
     return 'No Entry Found'
   } else {
-  return dailySteps.numSteps
+  return userActivityEntry.numSteps
   }
 }
 
 const getActiveMinutes = (id, day, dataList) => {
-  let activityLog = dataList.activityData 
-  let activeMinutes = activityLog.find(log => log.userID === id && log.date === day)
-  if (!activeMinutes) {
+  let userActivityEntry = getUserActivtyLog(id, day, dataList)
+  if (!userActivityEntry) {
     return 'No Entry Found'
   } else {
-  return activeMinutes.minutesActive
+  return userActivityEntry.minutesActive
   }
+
 }
 
 const getDailyMilesWalked = (id, day, dataList1, dataList2) => {
   let userLog = dataList1.users
   let userStrideData = userLog.find(log => id === log.id)
   let userStride = userStrideData.strideLength
-  let activityLog = dataList2.activityData
-  let userStepData = activityLog.find(log => log.userID === id && log.date === day)
-  
-  if (!userStepData) {
+  let userActivityEntry = getUserActivtyLog(id, day, dataList2)
+
+  if (!userActivityEntry) {
     return 'No Entry Found'
-  } else {
-  let userDailyStep = userStepData.numSteps
-  let walkedMiles = (userStride * userDailyStep)/5280
-  return Number(walkedMiles.toFixed(2))
-  }
+   } else {
+   let userDailyStep = userActivityEntry.numSteps
+   let walkedMiles = (userStride * userDailyStep)/5280
+   return Number(walkedMiles.toFixed(2))
+   }
+
 }
 
 const checkIfStepGoalWasMade = (id, day, dataList1, dataList2) => {
   let userLog = dataList1.users
   let userData = userLog.find(log => id === log.id)
   let userStepGoal = userData.dailyStepGoal
-  let activityLog = dataList2.activityData
 
-  let userStepData = activityLog.find(log => log.userID === id && log.date === day)
-  if (!userStepData) {
+  let userActivityEntry = getUserActivtyLog(id, day, dataList2)
+  if (!userActivityEntry) {
     return 'Invalid Argument'
   }
-  let userDailyStep = userStepData.numSteps
-
+  let userDailyStep = userActivityEntry.numSteps
   if (userDailyStep >= userStepGoal) {
     return `<span role="img" aria-label="check" title="check">✔</span>`
   } else {
@@ -58,4 +62,5 @@ export {
   getDailySteps,
   getActiveMinutes,
   getDailyMilesWalked,
+  getUserActivtyLog,
 }
